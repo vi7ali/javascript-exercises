90f174f74a49e2974927cf34de96a8f681d0374e
Vitaly Osipov <vi7ali@gmail.com>
2018-11-03 18:33:14 +0300

    Hello World and Repeat String exercises done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(repeatingString, numberOfTimes) {
+	
+	let tempString = repeatingString;
+	if(numberOfTimes>0) {
+		for(let i=0;i<numberOfTimes-1;i++) {
+			repeatingString = repeatingString+tempString;
+		}
+	}
+	else if(numberOfTimes<0)	{
+		repeatingString = 'ERROR';
+	}
+	else {
+		repeatingString='';
+	}
 
+	return repeatingString;
 }
 
 module.exports = repeatString
