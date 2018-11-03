a5f9ae917b4c36c95142f29ee4a590ce8875c501
Vitaly Osipov <vi7ali@gmail.com>
2018-11-03 19:03:28 +0300

    Completed the Reverse String exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+	/*1. Create an array with letter from the phrase
+		2. Create an empty resulting string	  
+	  3. Use loop to get from the end to start of the array
+	  4. In every iteration of the array push each letter to the end of the resulting string
+	  5. Return resulting string*/
+
+	  let letters = phrase.split("");
+	  let reversedString = '';
+
+	  for(let i=letters.length-1; i>=0; i--) {
+	  	reversedString += letters[i];
+	  }
+
+	  return reversedString;
 
 }
 
