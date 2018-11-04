7fa191950ecceea6aa0cc8e3b47f091e42e05ad5
Vitaly Osipov <vi7ali@gmail.com>
2018-11-04 17:52:46 +0300

    sumAll exercise done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+	let sum = 0;
+	if (startNumber>endNumber) {
+		let temp = startNumber;
+		startNumber = endNumber;
+		endNumber = temp;
+	}
+
+	if(startNumber<0 || endNumber<0) {
+		return 'ERROR';
+	}
+
+	if(typeof startNumber!=='number' || typeof endNumber!=='number') {
+		return 'ERROR';
+	}
+
+	for (let i=startNumber; i<=endNumber; i++) {
+		sum+=i;
+	}
+
+	return sum;
 
 }
 
