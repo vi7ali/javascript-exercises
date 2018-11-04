0c43fb9e2944bbfa5d093cdb35880afd35855a9b
Vitaly Osipov <vi7ali@gmail.com>
2018-11-04 16:48:24 +0300

    removeFromArray exercise done

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+	let args = [...arguments];
+	let input = args[0];
 
+	for(let i=1;i<args.length;i++) {
+		for (let j=0; j<input.length; j++) {
+			if(input[j]==args[i]) {
+				input.splice(j, 1);
+			}
+		}		
+	}
+	return input;
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,9 +1,4 @@
 const reverseString = function(phrase) {
-	/*1. Create an array with letter from the phrase
-		2. Create an empty resulting string	  
-	  3. Use loop to get from the end to start of the array
-	  4. In every iteration of the array push each letter to the end of the resulting string
-	  5. Return resulting string*/
 
 	  let letters = phrase.split("");
 	  let reversedString = '';
